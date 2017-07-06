28f2024d63f67c5ac0f3e9f1b2b90682d64d8b37
James Moore <jameslmoore@gmail.com>
2017-07-05 22:02:19 -1000

    savepoint

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
+/* jshint esversion: 6 */
+
 const net = require('net');
+
+let client_list = [];
 
 const server = net.createServer((c) => {
   console.log(c.remoteAddress + " " + c.remotePort + " client connected");
+
+  client_list.push(c.socket);
+
+  c.on('data', function(data) {
+    process.stdout.write(data);
+
+  //socket.write("HTTP/1.1 200 OK");
+  });
 
   c.on('end', () => {
     console.log("disconnected");
@@ -9,11 +21,10 @@ const server = net.createServer((c) => {
 
 });
 
-
 server.on('error', (err) => {
   throw err;
 });
 
-server.listen('8080', () => {
+server.listen('8080', '0.0.0.0', () => {
   console.log('server bound');
 });
