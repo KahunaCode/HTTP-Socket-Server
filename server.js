8d976571bd773d92b880c681d08cbadb4e57b610
James Moore <jameslmoore@gmail.com>
2017-07-06 18:06:02 -1000

    fixed rest of file access

--- server.js
+++ server.js
@@ -3,19 +3,15 @@
 const net = require('net');
 const fs = require('fs');
 
-let client_list = [];
-var htmlPages = ["index.html", "helium.html", "hydrogen.html"];
+let htmlPages = ["index.html", "helium.html", "hydrogen.html", "css/styles.css"];
 
 const server = net.createServer((c) => {
   console.log(c.remoteAddress + " " + c.remotePort + " client connectedn\n");
 
-  client_list.push(c.socket);
-
   c.on('data', function(data) {
     process.stdout.write(data);
-    var uri = data.toString().split('\n')[0].split(' ')[1].slice(1);//data buffer to string, split new lines, split on space, then remove '/'
+    let uri = data.toString().split('\n')[0].split(' ')[1].slice(1);//data buffer to string, split new lines, split on space, then remove '/'
     console.log("uri is", uri);
-
 
   if (htmlPages.includes(uri)) {
     var page = fs.readFile(uri, (err, data) => {
@@ -26,15 +22,13 @@ const server = net.createServer((c) => {
     });
   }
   else{
-    console.log("ohsht");
+    console.log("oh sht 404");
     var fourOhFour = fs.readFile("404.html", (err, data) => {
-          c.write("HTTP/1.1 200 OK\n Server: nginjames/1.4.6 (Macbuntu)\n Date: Wed, 05 Jul 2017 22:32:15 GMT\n Content-Type: text/html; charset=utf-8\n Content-Length: 40489\n Connection: keep-alive"+"\n\n"+data.toString());
-        });
-
+      c.write("HTTP/1.1 200 OK\n Server: nginjames/1.4.6 (Macbuntu)\n Date: Wed, 05 Jul 2017 22:32:15 GMT\n Content-Type: text/html; charset=utf-8\n Content-Length: 40489\n Connection: keep-alive"+"\n\n"+data.toString());
+      c.destroy();
+    });
   }
 });
-
-
 
   c.on('end', () => {
     console.log("disconnected");
